11535aa1eaf44e56569cefe9af5005e759fec481
Riska Andriani <randriani334@gmail.com>
2025-05-06 13:59:21 +0700

    menambah value end search bar

--- admin/kategori.php
+++ admin/kategori.php
@@ -53,20 +53,20 @@
  
     <div class="search-bar">
       <form class="search-form d-flex align-items-center" method="POST" action="#">
-        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+        <input type="text" name="query" placeholder="Search" title="Enter search keyword" value="<?php echo isset($_POST['query']) ? htmlspecialchars ($_POST['query']) : ''; ?>">
         <button type="submit" title="Search"><i class="bi bi-search"></i></button>
       </form>
     </div><!-- End Search Bar --> 
 
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
-        <li class="nav-item dropdown pe-3">
-
-        <li class="nav-item d-block d-lg-none">
+      <li class="nav-item d-block d-lg-none">
           <a class="nav-link nav-icon search-bar-toggle" href="#">
             <i class="bi bi-search"></i>
           </a>
         </li><!-- End Search Icon -->
+        <li class="nav-item dropdown pe-3">
+
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/2.jpg" alt="Profile" class="rounded-circle">
