276a312e05312ae5b7fb71bf5001a893c353ad69
Riska Andriani <randriani334@gmail.com>
2025-05-09 17:04:27 +0700

    edit footer di file produk.php

--- admin/produk.php
+++ admin/produk.php
@@ -268,7 +268,7 @@
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
         <div class="copyright">
-            &copy; Copyright <strong><span>Nama Website</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>ezone</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
             Designed by <a href="https://www.instagram.com/rskaaadr">Riska Andriani</a>
