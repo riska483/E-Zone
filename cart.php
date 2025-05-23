577818cfbc73802c93ba84f46685a5ce24c7e914
Riska Andriani <randriani334@gmail.com>
2025-05-23 21:33:06 +0700

    menambah update cart

--- cart.php
+++ cart.php
@@ -224,6 +224,26 @@ session_start();
                         <?php
                         include "admin/koneksi.php";
 
+                        if (isset($_POST['update_cart'])) {
+                            if (!isset($_SESSION['id_user'])) {
+                                echo "<script>alert('User tidak ditemukan!'); window.location='cart.php';</script>";
+                                exit;
+                            }
+                        
+                            $qtys = $_POST['qty'];
+                            foreach ($qtys as $id_pesanan => $qty) {
+                                $qty = (int)$qty;
+                                if ($qty < 1) $qty = 1;
+                        
+                                
+                                mysqli_query($koneksi, "UPDATE tb_pesanan SET qty = '$qty' WHERE id_pesanan = '$id_pesanan'");
+                            }
+                        
+                            echo "<script>alert('Keranjang berhasil diperbarui!'); window.location='cart.php';</script>";
+                            exit;
+                        }
+                        
+
                         if (isset($_POST['checkout'])) {
                             if (!isset($_SESSION['id_user'])) {
                                 echo "<script>alert('User tidak ditemukan!'); window.location='cart.php';</script>";
@@ -233,8 +253,7 @@ session_start();
                             $id_user = $_SESSION['id_user'];
 
                             // Ambil data pesanan user
-                            $query_pesanan = mysqli_query($koneksi, "
-                                SELECT p.*, pr.harga 
+                            $query_pesanan = mysqli_query($koneksi, "SELECT p.*, pr.harga 
                                 FROM tb_pesanan p
                                 JOIN tb_produk pr ON p.id_produk = pr.id_produk
                                 WHERE p.id_user = '$id_user'
@@ -362,7 +381,7 @@ session_start();
             <td class='quantity'>
                 <label>Quantity</label>
                 <div class='cart-plus-minus'>
-                    <input class='cart-plus-minus-box' value='{$row['qty']}' type='text' readonly>
+                    <input name='qty[{$row['id_pesanan']}]' class='cart-plus-minus-box' value='{$row['qty']}' type='number' min='1'>
                     <div class='dec qtybutton'><i class='fa fa-angle-down'></i></div>
                     <div class='inc qtybutton'><i class='fa fa-angle-up'></i></div>
                 </div>
